703f4c9cca01e462f61a8acd724b1cbc8252dc42
Tom Johnson <tomjohnson1634@gmail.com>
2022-05-12 17:29:28 +0100

    Initial Commit

---
+++ Harry Potter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Harry Potter
+//
+//  Created by Tom Johnson on 12/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Harry Potter/Harry_PotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harry_PotterApp.swift
+//  Harry Potter
+//
+//  Created by Tom Johnson on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Harry_PotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
